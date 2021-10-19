f7a511564b75c069d5e35feb95d9c2845e6d10e2
ZarakiLancelot <kenpachi2783@gmail.com>
2021-10-18 23:07:50 -0600

    layout component

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'sidenav',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  public isScreenSmall!: boolean;
+
+  constructor(private breakpoinObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.breakpoinObserver
+      // .observe([ Breakpoints.XSmall ])
+      .observe([ `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)` ])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
   }
 
 }
